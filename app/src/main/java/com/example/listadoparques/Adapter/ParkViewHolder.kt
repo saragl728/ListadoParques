37a6b992a35b959e0022daf18bdbecd669942b52
Usuario <saragl728@gmail.com>
2023-11-02 12:49:45 +0100

    Añado algo más a ParkViewHolder

--- app/src/main/java/com/example/listadoparques/Adapter/ParkViewHolder.kt
+++ app/src/main/java/com/example/listadoparques/Adapter/ParkViewHolder.kt
@@ -2,7 +2,12 @@ package com.example.listadoparques.Adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.listadoparques.Parques
+import com.example.listadoparques.databinding.ItemParqueBinding
 
 class ParkViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+val binding = ItemParqueBinding.bind(view)
+    fun render(parkModel: Parques){
 
+    }
 }
