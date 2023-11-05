9aabd7a786c85b83e5b3b6d75f155350bf54820d
Usuario <saragl728@gmail.com>
2023-11-05 22:45:24 +0100

    Añado cosas al ParkViewHolder

--- app/src/main/java/com/example/listadoparques/Adapter/ParkViewHolder.kt
+++ app/src/main/java/com/example/listadoparques/Adapter/ParkViewHolder.kt
@@ -1,13 +1,22 @@
 package com.example.listadoparques.Adapter
 
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.listadoparques.Parques
 import com.example.listadoparques.databinding.ItemParqueBinding
 
 class ParkViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 val binding = ItemParqueBinding.bind(view)
     fun render(parkModel: Parques){
+        binding.DescripcionParque.text = parkModel.descripcion
+        binding.nombreParque.text = parkModel.nombre
+        Glide.with(binding.fotoParque.context).load(parkModel.direccionImagen).into(binding.fotoParque)
 
+        //evento de hacer click en el parque
+        itemView.setOnClickListener{
+            Toast.makeText(binding.fotoParque.context, parkModel.nombre, Toast.LENGTH_LONG).show()
+        }
     }
 }
