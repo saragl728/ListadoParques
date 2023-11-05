1d27ec9752223612d0035635b7c06879d871b7ba
Usuario <saragl728@gmail.com>
2023-11-05 22:13:45 +0100

    Añado cosas al MainActivity

--- app/src/main/java/com/example/listadoparques/MainActivity.kt
+++ app/src/main/java/com/example/listadoparques/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.listadoparques
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.listadoparques.Adapter.ParkAdapter
 import com.example.listadoparques.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recycler.layoutManager = LinearLayoutManager(this)
+        binding.recycler.Adapter = ParkAdapter(ListaParques.listaDeParques)
 
     }
 }
