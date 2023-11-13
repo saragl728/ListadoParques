18a3a02be68a20fedf093203ab99b82e373ef5e8
Usuario <saragl728@gmail.com>
2023-11-13 10:47:49 +0100

    Añado un filtro de búsqueda

--- app/src/main/java/com/example/listadoparques/Adapter/ParkAdapter.kt
+++ app/src/main/java/com/example/listadoparques/Adapter/ParkAdapter.kt
@@ -1,12 +1,12 @@
 package com.example.listadoparques.Adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import android.view.LayoutInflater
 import com.example.listadoparques.Parques
 import com.example.listadoparques.R
 
-class ParkAdapter(val listaDeParques: List<Parques>): RecyclerView.Adapter<ParkViewHolder>() {
+class ParkAdapter(var listaDeParques: List<Parques>): RecyclerView.Adapter<ParkViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParkViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return ParkViewHolder(layoutInflater.inflate(R.layout.item_parque, parent, false))
@@ -19,5 +19,10 @@ class ParkAdapter(val listaDeParques: List<Parques>): RecyclerView.Adapter<ParkV
     override fun onBindViewHolder(holder: ParkViewHolder, position: Int) {
         val item = listaDeParques[position]
         holder.render(item)
+    }
+
+    fun actualizarParques(listaparque: List<Parques>) {
+        this.listaDeParques = listaparque
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/listadoparques/MainActivity.kt
+++ app/src/main/java/com/example/listadoparques/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.listadoparques
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listadoparques.Adapter.ParkAdapter
 import com.example.listadoparques.databinding.ActivityMainBinding
@@ -10,11 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
+        var adapter = ParkAdapter(ListaParques.listaDeParques)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.recycler.layoutManager = LinearLayoutManager(this)
-        binding.recycler.adapter = ParkAdapter(ListaParques.listaDeParques)
+        binding.recycler.adapter = adapter
+
+        binding.filtro.addTextChangedListener { filtro ->
+            val filtroParque =
+                ListaParques.listaDeParques.filter { parques ->
+                    parques.nombre.lowercase().contains(filtro.toString().lowercase())
+                }
+            adapter.actualizarParques(filtroParque)
+        }
 
     }
 }
